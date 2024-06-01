da6f110d98369e186511afc430139efe9258c22f
Siddhant Rohila <150060049+paneerpyaz@users.noreply.github.com>
2024-06-01 23:09:43 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,22 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, message);
-    });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed");
 });
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "saveAnnotation") {
+    try {
+      chrome.storage.local.set({ annotations: message.annotat ,highlights : message.high}, () => {
+        sendResponse({ status: "success" });
+      });
+    } catch (error) {
+      console.error("Error saving annotations:", error);
+      sendResponse({ status: "failure" });
+    }
+    return true; // Indicates that sendResponse will be called asynchronously
+  } else if (message.action === "loadAnnotations") {
+      chrome.storage.local.get(['annotations','highlights'], (result) => {
+      sendResponse({ annotations: result.annotations , highlights: result.highlights});
+    });
+    return true; // Indicates that sendResponse will be called asynchronously
+  }
 });
