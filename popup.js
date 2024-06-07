c2ef644318989c2e362dd31b493f84bed421e6ee
Siddhant Rohila <150060049+paneerpyaz@users.noreply.github.com>
2024-06-07 23:20:37 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -12,13 +12,27 @@ document.getElementById('highlight-btn').addEventListener('click',()=>{
 document.getElementById('save-btn').addEventListener('click',()=>{
   chrome.tabs.query({active: true,currentWindow: true},(tabs)=>{
     chrome.tabs.sendMessage(tabs[0].id,{action: "save-btn"});
-  });
+  }); 
 }); 
-document.getElementById('undo-btn').addEventListener('click',()=>{
-  chrome.tabs.query({active: true,currentWindow: true},(tabs)=>{
-    chrome.tabs.sendMessage(tabs[0].id,{action: "undo-btn"});
-  });
+let intervalId = null;
+
+document.getElementById('undo-btn').addEventListener('mousedown', () => {
+    // intervalId = setInterval(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "undo-btn" });
+        });
+    // }, 50); // Adjust the interval as needed for the speed of repeating action
 });
+
+// document.getElementById('undo-btn').addEventListener('mouseup', () => {
+//     clearInterval(intervalId);
+// });
+
+// document.getElementById('undo-btn').addEventListener('mouseleave', () => {
+//     clearInterval(intervalId);
+// });
+
+
 document.getElementById('highlight-btn').addEventListener('click', () => {
   document.getElementById('color-container').style.display = 'flex';
 });
@@ -44,3 +58,4 @@ document.addEventListener('DOMContentLoaded', function () {
     colorContainer.style.display = 'none';
   });
 });
+
